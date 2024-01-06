2a90f459d16cce13ac08fde47dbec66834b9b5fa
Andrei <a.muravjev1515@yndex.ru>
2024-01-06 15:26:30 +0300

    functions

--- cycle_for.py
+++ cycle_for.py
@@ -6,3 +6,21 @@ for stamp in car_list:
     cars_count += 10
     pass
 print('Всего автомобилей', cars_count)
+
+# Параметры
+def print_params(color_car):
+    print('Авто:', color_car)
+my_car = [('BMW - red'), ('LADA - black')]
+print('Цвета моих машин')
+for color in my_car:
+    print_params(color_car=color)
+    print('Все правильно')
+
+
+def print_params(color_car):
+    color_car = 'BMW - red, LADA - black'
+    print('Цвет авто:', color_car)
+
+
+for color_car in range(2):
+    print_params(color_car)
